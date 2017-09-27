09632a2d5db80f0007bdf43657888006a73b385f
OY <objecttozero@sina.com>
2017-09-27 14:55:48 +0800

    add local search

--- source/js/hannah.js
+++ source/js/hannah.js
@@ -9,6 +9,88 @@
         footer.classList.remove("hide");
     }
 
+    function ajax(url, type, callback) {
+        var xmlHttp = null;
+        if (win.XMLHttpRequest) {// code for IE7, Firefox, Opera, etc.
+            xmlHttp = new XMLHttpRequest();
+        }
+        else if (win.ActiveXObject) {// code for IE6, IE5
+            xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        if (xmlHttp != null) {
+            xmlHttp.onreadystatechange = function () {
+                callback(xmlHttp);
+            };
+            xmlHttp.open(type, url, true);
+            xmlHttp.send(null);
+        }
+        else {
+            console.error("Your browser does not support XMLHTTP.");
+        }
+    }
+
+    var searchData;
+    var timeOutSearch;
+
+    function searchKey(ele) {
+        var val = ele.value;
+        ele.nextElementSibling.innerHTML = "<p style='text-align: center;line-height: 300%'>暂无数据</p>";
+        searchData.filter(function (t) {
+            return t.title && t.title.toLowerCase().indexOf(val.trim().toLowerCase()) >= 0;
+        }).forEach(function (t, i) {
+            if (i === 0)
+                ele.nextElementSibling.innerHTML = "";
+            var ele_a = document.createElement("a");
+            ele_a.url = t.url;
+            ele_a.innerHTML = t.title;
+            ele_a.classList.add("search-result-item");
+            ele.nextElementSibling.appendChild(ele_a);
+        });
+    }
+
+
+    function search(ele, event, url) {
+        if (ele.value && ele.value.trim()) {
+            clearTimeout(timeOutSearch);
+            timeOutSearch = setTimeout(function () {
+                if (searchData)
+                    searchKey(ele);
+                else
+                    ajax(url, "GET", function (xmlHttp) {
+                        if (xmlHttp.readyState == 4) {// 4 = "loaded"
+                            if (xmlHttp.status == 200) {// 200 = OK
+                                var entries = xmlHttp.responseXML.getElementsByTagName("entry");
+                                searchData = [];
+                                for (var i = 0; i < entries.length; i++) {
+                                    var entry = entries[i];
+                                    var categories = [], tags = [];
+                                    entry.getElementsByTagName("categories").item(0).childNodes.forEach(function (t) {
+                                        if (t.tagName === "category" && t.textContent)
+                                            categories.push(t.textContent.trim());
+                                    });
+                                    entry.getElementsByTagName("tags").item(0).childNodes.forEach(function (t) {
+                                        if (t.tagName === "tag" && t.textContent)
+                                            tags.push(t.textContent.trim());
+                                    });
+                                    searchData.push({
+                                        title: entry.getElementsByTagName("title").item(0).textContent,
+                                        content: entry.getElementsByTagName("content").item(0).textContent.replace(/<[^>]+>/g, ""),
+                                        url: entry.getElementsByTagName("url").item(0).textContent,
+                                        categories: categories,
+                                        tags: tags
+                                    });
+                                }
+                                searchKey(ele);
+                            }
+                            else {
+                                console.error("Problem retrieving XML data");
+                            }
+                        }
+                    });
+            }, 500);
+        }
+    }
+
     win.onload = function () {
         var btnMenu = document.getElementById("btn_menu");
         if (btnMenu) {
@@ -28,4 +110,6 @@
         }, 500);
 
     }
+
+    win.searchFunc = search;
 })(window);
